c0c103b1cada4656a4d691e4c74d618d439d80b1
Harish <harishpoctor@gmail.com>
2022-08-05 12:16:49 +0530

    Section 7 : Forms : Handling Form Submission completed

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import NavBar from "./components/common/navBar";
 import Movies from "./components/movies";
@@ -5,8 +6,8 @@ import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
 import MovieForm from "./components/movieForm";
+import LoginForm from "./components/loginForm";
 import "./App.css";
-import React from "react";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <NavBar />
       <main className="container">
         <Switch>
+          <Route path="/login" component={LoginForm} />
           <Route path="/movies/:id" component={MovieForm} />
           <Route path="/movies" component={Movies} />
           <Route path="/customers" component={Customers} />
