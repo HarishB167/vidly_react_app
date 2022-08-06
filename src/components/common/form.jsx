8d161d6199189764e461932f1ef2c48590b30489
Harish <harishpoctor@gmail.com>
2022-08-06 08:48:40 +0530

    Section 7 : Forms : Extracting Helper Rendering Methods - completed

--- src/components/common/form.jsx
+++ src/components/common/form.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Joi from "joi-browser";
+import Input from "./input";
 
 class Form extends Component {
   state = {
@@ -46,6 +47,28 @@ class Form extends Component {
     const data = { ...this.state.data };
     data[input.name] = input.value;
     this.setState({ data, errors });
+  };
+
+  renderSubmit = (label) => {
+    return (
+      <button disabled={this.validate()} className="btn btn-primary">
+        {label}
+      </button>
+    );
+  };
+
+  renderInput = (name, label, type = "text") => {
+    const { data, errors } = this.state;
+    return (
+      <Input
+        name={name}
+        label={label}
+        type={type}
+        value={data[name]}
+        error={errors[name]}
+        onChange={this.handleChange}
+      />
+    );
   };
 }
 
